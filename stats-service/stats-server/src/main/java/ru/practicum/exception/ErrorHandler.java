d70d5f24dcd40cab9d01d336c75da85285a4e70d
swwwer82 <swwwer82@yandex.ru>
2024-08-05 12:18:56 +0300

    немного первых правок

--- stats-service/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ stats-service/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -7,23 +8,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequest(final MethodArgumentTypeMismatchException exception) {
+        log.error("Handling MethodArgumentTypeMismatchException: {}", exception.getMessage(), exception);
         return new ErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(final Exception exception) {
+        log.error("Handling generic exception: {}", exception.getMessage(), exception);
         return new ErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleException(final BadRequestException exception) {
+        log.error("Handling BadRequestException: {}", exception.getMessage(), exception);
         return new ErrorResponse(exception.getMessage());
     }
 }
